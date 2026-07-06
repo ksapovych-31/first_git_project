4efc8f4f47861d282b391d7aa00c40bcfc33aa0e
ksapovych-31 <ksapovych@gmail.com>
2026-07-06 20:51:58 +0300

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+        System.out.println("Hello CYDEO");
+        System.out.println("Hello Programmers");
     }
 }
